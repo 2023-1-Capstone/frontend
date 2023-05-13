751040065a669d1b32fe8f5743b06605f0914446
jsl0149 <jsl0149@naver.com>
2023-05-14 04:09:11 +0900

    refactor : 계절별 순위 페이지에 필요한 함수 분리

--- src/pages/Indicate/Season/util.ts
+++ src/pages/Indicate/Season/util.ts
@@ -6,11 +6,35 @@ const getAverageFee = (targetFeeArray: any, targetSeasonIdx: number) => {
       if (targetFeeArray)
         return (
           acc +
-          Math.floor(targetFeeArray[cur]?.fee / targetFeeArray[cur]?.usages)
+          Math.floor(
+            targetFeeArray[cur]?.fee / (targetFeeArray[cur]?.usages * 1000)
+          )
         );
       return 0;
     }, 0) / 3
   );
 };
 
-export { getAverageFee };
+const findMostWasteIdx = (chart: any) => {
+  return chart[chart.length - 1].usages.reduce(
+    (iMax: number, x: number, idx: number, arr: number[]) =>
+      x > arr[iMax] ? idx : iMax,
+    0
+  );
+};
+
+const getRandomNumber = (max: number, min = 0) => {
+  return Math.floor(Math.random() * max) + min;
+};
+
+const getUniqueNumberList = (count: number, max: number, min = 0) => {
+  const list: Set<number> = new Set<number>();
+
+  while (count > list.size) {
+    list.add(getRandomNumber(max, min));
+  }
+
+  return Array.from(list);
+};
+
+export { getAverageFee, findMostWasteIdx, getUniqueNumberList };
