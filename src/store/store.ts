5800844e9b980133066d029eb553d7aab7276d47
jsl0149 <jsl0149@naver.com>
2023-05-23 14:08:01 +0900

    feat : 도넛 차트 outlabel 표기 기능 추가, 텍스트 중앙 표기 기능 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -12,6 +12,72 @@ import { buildingInfoType } from '../type/Types';
 import electricityIcon from '../assets/svg/electricityCategory.svg';
 import gasIcon from '../assets/svg/gasCategory.svg';
 import carbon from '../assets/svg/carbon.svg';
+import 'chartjs-plugin-doughnutlabel-rebourne';
+
+const plugin = {
+  id: 'emptyDoughnut',
+  afterDraw(chart: any, args: any, options: any) {
+    const {
+      ctx,
+      chartArea: { top, bottom, left, right, width, height },
+    } = chart;
+    let total = 0;
+    chart.data.datasets.forEach((dataset: any, idx: number) => {
+      total = dataset.data?.reduce((acc: number, cur: number) => acc + cur, 0);
+      chart.getDatasetMeta(idx).data.forEach((datapoint: any, index: any) => {
+        const { x, y } = datapoint.tooltipPosition();
+
+        const halfwidth = width / 2;
+        const halfheight = height / 2;
+
+        const xLine = x >= halfwidth ? x + 20 : x - 20;
+        const yLine = y >= halfwidth ? y + 30 : y - 30;
+        const extraLine = x >= halfwidth ? 15 : -15;
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        if (chart.data.labels[index] === '로스쿨관') {
+          ctx.lineTo(xLine, yLine - 10);
+          ctx.lineTo(xLine + extraLine, yLine - 10);
+        } else {
+          ctx.lineTo(xLine, yLine);
+          ctx.lineTo(xLine + extraLine, yLine);
+        }
+
+        ctx.strokeStyle = dataset.borderColor[index];
+        ctx.stroke();
+
+        //text
+        ctx.font = 'bold 13px Arial';
+        //position
+        const textXPosition = x >= halfwidth ? 'left' : 'right';
+        const plusFivePx = x >= halfwidth ? 5 : -5;
+        ctx.textAlign = textXPosition;
+
+        ctx.textBaseLine = 'middle';
+        ctx.fillStyle = dataset.borderColor[index];
+
+        if (chart.data.labels[index] === '로스쿨관')
+          ctx.fillText(chart.data.labels[index], xLine + extraLine, yLine - 10);
+        else ctx.fillText(chart.data.labels[index], xLine + extraLine, yLine);
+      });
+    });
+    ctx.save();
+    const xCoor = chart.getDatasetMeta(0).data[0]?.x;
+    const yCoor = chart.getDatasetMeta(0).data[0]?.y;
+    ctx.fillStyle = '#000';
+    ctx.textAlign = 'center';
+    ctx.textBaseLine = 'middle';
+    ctx.font = 'bold 15px Pretendard';
+    ctx.fillStyle = '#92C111';
+    ctx.fillText('총 탄소 배출량', xCoor, yCoor - 10);
+    ctx.font = 'bold 20px Pretendard';
+    ctx.borderColor = '#757575';
+    ctx.fillStyle = '#757575';
+    ctx.fillText(total.toLocaleString('ko-KR'), xCoor - 10, yCoor + 20);
+    ctx.fillText('kg', xCoor + 47, yCoor + 20);
+  },
+};
 
 const monthlyInitData: any = {
   labels: [
@@ -32,7 +98,7 @@ const monthlyInitData: any = {
     {
       backgroundColor: ['rgb(75, 192, 192)'],
       maxBarThickness: 35,
-      borderRadius: 7,
+      borderRadius: 3,
       data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     },
   ],
@@ -56,8 +122,6 @@ const monthlyInitDatas: any = {
   datasets: [
     {
       backgroundColor: ['rgb(75, 192, 192)'],
-      maxBarThickness: 35,
-      borderRadius: 7,
       data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     },
   ],
@@ -77,6 +141,8 @@ const seasonInitData: any = {
 };
 
 const areaInitData: any = {
+  plugins: [plugin],
+
   labels: [
     '본관',
     '하이테크관',
@@ -92,10 +158,85 @@ const areaInitData: any = {
 
   datasets: [
     {
-      type: 'bar',
       maxBarThickness: 35,
-      backgroundColor: 'rgb(75, 192, 192)',
-      data: [0],
+      backgroundColor: [
+        'rgba(0, 99, 132, 0.6)',
+        'rgba(25, 99, 132, 0.6)',
+        'rgba(50, 99, 132, 0.6)',
+        'rgba(75, 99, 132, 0.6)',
+        'rgba(100, 99, 132, 0.6)',
+        'rgba(125, 99, 132, 0.6)',
+        'rgba(150, 99, 132, 0.6)',
+        'rgba(175, 99, 132, 0.6)',
+        'rgba(200, 99, 132, 0.6)',
+        'rgba(225, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+      ],
+      borderColor: [
+        'rgba(0, 99, 132, 0.6)',
+        'rgba(25, 99, 132, 0.6)',
+        'rgba(50, 99, 132, 0.6)',
+        'rgba(75, 99, 132, 0.6)',
+        'rgba(100, 99, 132, 0.6)',
+        'rgba(125, 99, 132, 0.6)',
+        'rgba(150, 99, 132, 0.6)',
+        'rgba(175, 99, 132, 0.6)',
+        'rgba(200, 99, 132, 0.6)',
+        'rgba(225, 99, 132, 0.6)',
+      ],
+      borderRadius: 3,
+      data: [],
+    },
+  ],
+};
+
+const carbonBuildingInitData: any = {
+  plugins: [plugin],
+
+  labels: [
+    '본관',
+    '하이테크관',
+    '정석',
+    '60주년',
+    '2,4호관',
+    '5호관',
+    '6,9호관',
+    '학생회관',
+    '서호관',
+    '로스쿨관',
+  ],
+
+  datasets: [
+    {
+      maxBarThickness: 10,
+      backgroundColor: [
+        'rgba(0, 99, 132, 0.6)',
+        'rgba(25, 99, 132, 0.6)',
+        'rgba(50, 99, 132, 0.6)',
+        'rgba(75, 99, 132, 0.6)',
+        'rgba(100, 99, 132, 0.6)',
+        'rgba(125, 99, 132, 0.6)',
+        'rgba(150, 99, 132, 0.6)',
+        'rgba(175, 99, 132, 0.6)',
+        'rgba(200, 99, 132, 0.6)',
+        'rgba(225, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+      ],
+      borderColor: [
+        'rgba(0, 99, 132, 0.6)',
+        'rgba(25, 99, 132, 0.6)',
+        'rgba(50, 99, 132, 0.6)',
+        'rgba(75, 99, 132, 0.6)',
+        'rgba(100, 99, 132, 0.6)',
+        'rgba(125, 99, 132, 0.6)',
+        'rgba(150, 99, 132, 0.6)',
+        'rgba(175, 99, 132, 0.6)',
+        'rgba(200, 99, 132, 0.6)',
+        'rgba(225, 99, 132, 0.6)',
+      ],
+      borderRadius: 3,
+      data: [],
+      borderWidth: 1,
     },
   ],
 };
@@ -207,9 +348,16 @@ const options: any = {
 };
 
 const optionsDoughnut: any = {
+  layout: {
+    padding: {
+      top: 40,
+      left: 10,
+      bottom: 30,
+    },
+  },
   plugins: {
     title: {
-      display: true,
+      display: false,
       text: '월별 에너지 사용량 비율',
     },
     tooltips: {
@@ -218,15 +366,91 @@ const optionsDoughnut: any = {
       },
     },
     datalabels: {
-      formatter: function (value: any, context: any) {
-        return `${Math.round(
-          (value / context.chart.getDatasetMeta(0).total) * 100
-        )}%`;
+      labels: {
+        index: {
+          font: {
+            size: 14,
+            weight: 'bold',
+          },
+          color: '#404040',
+          formatter: function (value: any, context: any) {
+            return context.chart.data.labels[context.dataIndex];
+          },
+          align: 'end',
+          anchor: 'end',
+        },
+        value: {
+          font: {
+            size: 12,
+            weight: 'bold',
+          },
+          color: '#fff',
+          formatter: function (value: any, context: any) {
+            const percentage =
+              Math.round(
+                (value / context.chart.getDatasetMeta(0).total) * 100
+              ) + '%';
+            return percentage;
+          },
+
+          align: 'middle',
+          anchor: 'middle',
+        },
       },
-      color: '#fff',
     },
     legend: {
-      font: {},
+      display: false,
+    },
+  },
+};
+
+const optionsDoughnutCarbon: any = {
+  layout: {
+    padding: {
+      top: 80,
+      left: 10,
+      bottom: 30,
+    },
+  },
+  cutout: '65%',
+  plugins: {
+    title: {
+      display: true,
+    },
+    tooltips: {
+      callbacks: {
+        title: (context: any) => context[0].label + '월',
+      },
+    },
+
+    datalabels: {
+      labels: {
+        // index: {
+        //   font: {
+        //     size: 14,
+        //     weight: 'bold',
+        //   },
+        //   color: '#404040',
+        //   formatter: function (value: any, context: any) {
+        //     return context.chart.data.labels[context.dataIndex];
+        //   },
+        //   align: 'end',
+        //   anchor: 'end',
+        // },
+        value: {
+          formatter: function (value: any, context: any) {
+            return `${Math.round(
+              (value /
+                context.chart.getDatasetMeta(context.datasetIndex).total) *
+                100
+            )}%`;
+          },
+          color: '#fff',
+        },
+      },
+    },
+    legend: {
+      display: false,
     },
   },
 };
@@ -373,6 +597,53 @@ const optionsCarbon: any = {
   },
 };
 
+const optionsCarbonBuilding: any = {
+  reponsive: false,
+  indexAxis: 'y',
+  plugins: {
+    datalabels: {
+      formatter: (value: any, context: any) => {
+        return value.toLocaleString('ko-KR') + 'kg';
+      },
+      color: '#fff',
+    },
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      callbacks: {
+        title: (context: any) => context[0].label,
+        label: (context: any) => {
+          let label = context.dataset.label + '' || '';
+          return context.parsed.y !== null
+            ? context.parsed.x.toLocaleString('ko-KR') + 'Kg'
+            : null;
+        },
+      },
+    },
+  },
+  scales: {
+    x: {
+      offset: false,
+      grid: {
+        display: false,
+      },
+      ticks: {
+        callback: function (val: any, index: any): any {
+          return val / 10000 + '만';
+        },
+      },
+    },
+    y: {
+      grid: {
+        display: false,
+      },
+
+      title: {},
+    },
+  },
+};
+
 const indicateCategory = [
   {
     content: '계절별 전력사용 순위',
@@ -421,18 +692,18 @@ const stuffPrice = {
 };
 
 const doughnutColor = [
-  'rgb(255, 0, 0, 0.5)',
-  'rgb(255, 94, 0, 0.5)',
-  'rgb(255, 187, 0, 0.5)',
-  'rgb(255, 228, 0, 0.5)',
-  'rgb(171, 242, 0, 0.5)',
-  'rgb(29, 219, 22, 0.5)',
-  'rgb(0, 216, 255, 0.5)',
-  'rgb(0, 84, 255, 0.5)',
-  'rgb(1, 0, 255, 0.5)',
-  'rgb(95, 0, 255, 0.5)',
-  'rgb(255, 0, 221, 0.5)',
-  'rgb(255, 0, 127, 0.5)',
+  'rgb(255, 0, 0, 0.4)',
+  'rgb(255, 94, 0, 0.4)',
+  'rgb(255, 187, 0, 0.4)',
+  'rgb(255, 228, 0, 0.4)',
+  'rgb(171, 242, 0, 0.4)',
+  'rgb(29, 219, 22, 0.4)',
+  'rgb(0, 216, 255, 0.4)',
+  'rgb(0, 84, 255, 0.4)',
+  'rgb(1, 0, 255, 0.4)',
+  'rgb(95, 0, 255, 0.4)',
+  'rgb(255, 0, 221, 0.4)',
+  'rgb(255, 0, 127, 0.4)',
 ];
 
 const season = ['봄', '여름', '가을', '겨울'];
@@ -459,4 +730,8 @@ export {
   optionsDoughnut,
   doughnutColor,
   optionsCarbon,
+  optionsCarbonBuilding,
+  optionsDoughnutCarbon,
+  plugin,
+  carbonBuildingInitData,
 };
