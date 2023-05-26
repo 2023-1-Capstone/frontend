ac51ff1d242c904913bd147ed47c43f966ed5dfc
jsl0149 <jsl0149@naver.com>
2023-05-26 18:56:30 +0900

    publish : 시작 페이지 로그인 추가

--- src/pages/Start/Start.style.tsx
+++ src/pages/Start/Start.style.tsx
@@ -44,7 +44,7 @@ const StartTitle = styled.div`
   /* Inside auto layout */
 
   flex: none;
-  order: 1;
+
   flex-grow: 0;
 `;
 
@@ -66,7 +66,7 @@ const StartDescription = styled.div`
   /* Inside auto layout */
 
   flex: none;
-  order: 2;
+
   flex-grow: 0;
 `;
 
@@ -88,8 +88,111 @@ const StartNext = styled.div`
 
   cursor: pointer;
   flex: none;
-  order: 3;
+
   flex-grow: 0;
 `;
 
-export { StartInner, SymbolMark, StartTitle, StartDescription, StartNext };
+const LoginInput = styled.input`
+  width: 19.3rem;
+  height: 3.1rem;
+  font-family: 'Pretendard';
+  background: #ffffff;
+  border-radius: 0.4rem;
+  border: 0px;
+  /* Inside auto layout */
+  text-align: left;
+  &:hover {
+    outline: none;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:placeholder {
+    color: blue;
+    font-size: 2.5em;
+  }
+  padding: 1rem;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const LoginBottomText = styled.span`
+  width: fit-content;
+  height: fit-content;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 250;
+  font-size: 1.2rem;
+
+  color: #ffffff;
+
+  /* Inside auto layout */
+  cursor: pointer;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const LoginBottomTextWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 19.3rem;
+  justify-content: space-between;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 19.3rem;
+  gap: 1.1rem;
+`;
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: 1.1rem;
+  position: relative;
+  width: 19.3rem;
+  height: fit-content;
+`;
+
+const LoginButton = styled.div`
+  position: relative;
+  width: 19.3rem;
+  height: 4.4rem;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* identical to box height */
+  cursor: pointer;
+  color: #0065b3;
+  top: 1rem;
+  background: #ffffff;
+  border-radius: 4px;
+`;
+
+export {
+  StartInner,
+  SymbolMark,
+  StartTitle,
+  StartDescription,
+  StartNext,
+  Form,
+  LoginInput,
+  LoginBottomText,
+  LoginWrapper,
+  LoginBottomTextWrapper,
+  LoginButton,
+};
--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { Wrapper, WrapperInner } from '../../components/Wrapper/Wrapper.style';
 import * as S from './Start.style';
 import Symbol from '../../assets/SymbolMark.jpg';
@@ -11,15 +12,45 @@ const StartPage = () => {
     navigator('/home');
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setError,
+  } = useForm({
+    mode: 'all',
+  });
+
+  useEffect(() => {
+    console.log(watch('nickname'));
+  }, [watch('nickname')]);
+
   return (
     <Wrapper color={'#0065B3'}>
       <S.StartInner>
         <S.SymbolMark src={Symbol}></S.SymbolMark>
+        <S.LoginWrapper>
+          <S.Form>
+            <S.LoginInput
+              placeholder={'이메일'}
+              {...register('email')}
+            ></S.LoginInput>
+            <S.LoginInput
+              placeholder={'비밀번호'}
+              type={'password'}
+              {...register('password')}
+            ></S.LoginInput>
+          </S.Form>
+          <S.LoginBottomTextWrapper>
+            <S.LoginBottomText onClick={() => navigator('/signup')}>
+              회원가입
+            </S.LoginBottomText>
+            <S.LoginBottomText>계정찾기</S.LoginBottomText>
+          </S.LoginBottomTextWrapper>
+          <S.LoginButton onClick={goHomePage}>로그인</S.LoginButton>
+        </S.LoginWrapper>
         <S.StartTitle>Carbon LIVE</S.StartTitle>
-        <S.StartDescription>
-          인하대학교의 에너지 사용량을 확인해보세요
-        </S.StartDescription>
-        <S.StartNext onClick={goHomePage}>시작하기</S.StartNext>
       </S.StartInner>
     </Wrapper>
   );
