22034330d4e72225337555f962fd80db9b326a09
jsl0149 <jsl0149@naver.com>
2023-06-04 14:18:16 +0900

    feat : 통합 탄소 페이지 라우팅 기능 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,22 +2,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
 import StartPage from '../pages/Start/Start';
 import HomePage from '../pages/Home/Home';
-import BuildingElectricity from '../pages/BuildingElectricity/BuildingElectricity';
-import BuildingGas from '../pages/BuildingGas/BuildingGas';
 import Indicate from '../pages/Indicate/Indicate';
-import SeasonEletricity from '../pages/Indicate/Season/electricity/SeasonElectricity';
-import SeasonGas from '../pages/Indicate/Season/gas/SeasonGas';
 import CarbonAll from '../pages/Indicate/Carbon/All/CarbonAll';
 import CarbonBuildings from '../pages/Indicate/Carbon/Buildings/CarbonBuildings';
-import AreaElectricity from '../pages/Indicate/Area/electricity/AreaElectricity';
-import AreaGas from '../pages/Indicate/Area/gas/AreaGas';
 import SignUp from '../pages/SignUp/SignUp';
 import FindPassword from '../pages/FindPassword/FindPassword';
 import ModifyPassword from '../pages/ModifyPassword/ModifyPassword';
 import Exception from '../pages/Exception/Exception';
 import Water from '../pages/Water/Water';
-import ElectricityAll from '../pages/ElectricityAll/ElectricityAll';
-import GasAll from '../pages/GasAll/GasAll';
+import Electricity from '../pages/Electricity/Electricity';
+import Gas from '../pages/Gas/Gas';
+import Carbon from '../pages/Carbon/Carbon';
 
 const Router = () => {
   return (
@@ -27,30 +22,18 @@ const Router = () => {
           <Route path="" element={<StartPage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/electricity" element={<BuildingElectricity />} />
-          <Route path="/gas" element={<BuildingGas />} />
-          <Route path="/indicator" element={<Indicate />} />
+          <Route path="/electricity" element={<Electricity />} />
+          <Route path="/gas" element={<Gas />} />
+          <Route path="/carbon" element={<Carbon />} />
           <Route path="/findPassword" element={<FindPassword />} />
           <Route path="/modifyPassword" element={<ModifyPassword />} />
-          <Route
-            path="/indicator/season/electricity"
-            element={<SeasonEletricity />}
-          />
-          <Route path="/indicator/season/gas" element={<SeasonGas />} />
           <Route path="/indicator/carbon/all" element={<CarbonAll />} />
           <Route
             path="/indicator/carbon/buildings"
             element={<CarbonBuildings />}
           />
-          <Route
-            path="/indicator/area/electricity"
-            element={<AreaElectricity />}
-          />
-          <Route path="/indicator/area/gas" element={<AreaGas />} />
           <Route path="/authException" element={<Exception />} />
           <Route path="/water" element={<Water />} />
-          <Route path="/electricityAll" element={<ElectricityAll />} />
-          <Route path="/gasAll" element={<GasAll />} />
         </Routes>
       </BrowserRouter>
     </>
